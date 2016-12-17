05e39eae7bb81c0f849b10f5c4f7a744230919aa
Sviridov Vladimir <v.shtille@gmail.com>
2016-12-17 23:13:53 +0300

    added thirdparty libraries

---
+++ thirdparty/libjpeg.mk
@@ -0,0 +1,68 @@
+# Makefile for Unix
+
+LIB_PATH = libjpeg/src
+
+include libjpeg-sources.mk
+
+TARGET = jpeg
+ROOT_PATH = ..
+TARGET_PATH = $(ROOT_PATH)/bin
+STATIC_LIB = lib$(TARGET).a
+SHARED_LIB = lib$(TARGET).so
+
+ifeq ($(IS_STATIC),NO)
+TARGET_TYPE = dynamic
+TARGET_FILE = $(SHARED_LIB)
+else
+TARGET_TYPE = static
+TARGET_FILE = $(STATIC_LIB)
+endif
+
+CC = gcc
+AR = ar rcs
+
+CP = cp
+RM = rm -f
+
+INCLUDE += -I$(LIB_PATH) -I$(LIB_PATH)/../include
+
+CFLAGS = -g -Wall -O3 -std=c99
+CFLAGS += $(INCLUDE)
+CFLAGS += $(DEFINES)
+
+LDFLAGS = -shared -fPIC
+
+OBJECTS = $(SRC_FILES:.c=.o)
+
+LIBRARIES =
+
+INSTALL_PATH = $(TARGET_PATH)
+
+all: $(SRC_FILES) $(TARGET)
+	@echo All is done!
+
+$(TARGET): create_dir clean $(TARGET_TYPE) install
+
+create_dir:
+	@test -d $(TARGET_PATH) || mkdir $(TARGET_PATH)
+
+clean:
+	@find $(LIB_PATH) -name "*.o" -type f -delete
+
+install:
+	@echo installing to $(INSTALL_PATH)
+	@$(RM) $(INSTALL_PATH)/$(TARGET_FILE)
+	@$(CP) $(TARGET_FILE) $(INSTALL_PATH)/$(TARGET_FILE)
+	@$(RM) $(TARGET_FILE)
+    
+static: $(OBJECTS)
+	@echo making static library
+	@$(AR) $(STATIC_LIB) $(OBJECTS)
+	
+dynamic: $(OBJECTS)
+	@echo making shared library
+	@$(CC) $(LDFLAGS) -o $(SHARED_LIB) $^ $(LIBRARIES)
+
+%.o : %.c
+	@echo compiling file $<
+	@$(CC) $(CFLAGS) -c $< -o $@
---
+++ thirdparty/libpng-sources.mk
@@ -0,0 +1,16 @@
+SRC_FILES = \
+        $(LIB_PATH)/png.c      \
+        $(LIB_PATH)/pngerror.c \
+        $(LIB_PATH)/pngget.c   \
+        $(LIB_PATH)/pngmem.c   \
+        $(LIB_PATH)/pngpread.c \
+        $(LIB_PATH)/pngread.c  \
+        $(LIB_PATH)/pngrio.c   \
+        $(LIB_PATH)/pngrtran.c \
+        $(LIB_PATH)/pngrutil.c \
+        $(LIB_PATH)/pngset.c   \
+        $(LIB_PATH)/pngtrans.c \
+        $(LIB_PATH)/pngwio.c   \
+        $(LIB_PATH)/pngwrite.c \
+        $(LIB_PATH)/pngwtran.c \
+        $(LIB_PATH)/pngwutil.c
