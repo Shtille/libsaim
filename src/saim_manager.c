e71dafde96263b60522cba91a91cef614571aa37
Shtille <v.shtille@gmail.com>
2019-06-27 01:18:38 +0300

    fixed crash issue
    crash was caused by early rasterizer deinitialization

--- src/saim_manager.c
+++ src/saim_manager.c
@@ -47,31 +47,32 @@ bool saim_manager__initialize_cache(const char* hash_string)
 	if (!saim_provider__create(s_provider))
 		return false;
 
+	s_rasterizer = (saim_rasterizer *) SAIM_MALLOC(sizeof(saim_rasterizer));
+	if (!saim_rasterizer__create(s_rasterizer))
+		return false;
+
 	s_cache = (saim_cache *) SAIM_MALLOC(sizeof(saim_cache));
 	if (!saim_cache__create(s_cache, hash_string))
 		return false;
 	if (!saim_cache__initialize(s_cache))
 		return false;
 
-	s_rasterizer = (saim_rasterizer *) SAIM_MALLOC(sizeof(saim_rasterizer));
-	if (!saim_rasterizer__create(s_rasterizer))
-		return false;
-
 	return true;
 }
 void saim_manager__deinitialize_cache()
 {
-	if (s_rasterizer)
-	{
-		saim_rasterizer__destroy(s_rasterizer);
-		SAIM_FREE(s_rasterizer);
-		s_rasterizer = NULL;
-	}
 	if (s_cache)
 	{
 		saim_cache__destroy(s_cache);
 		SAIM_FREE(s_cache);
 		s_cache = NULL;
+	}
+	// Rasterizer should be deinitialized after cache due to notification calls
+	if (s_rasterizer)
+	{
+		saim_rasterizer__destroy(s_rasterizer);
+		SAIM_FREE(s_rasterizer);
+		s_rasterizer = NULL;
 	}
 	if (s_provider)
 	{
